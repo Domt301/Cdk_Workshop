3f14c545ea2e9b2c7f98577846ad064df74c5d0d
Leonardo Wildt <LWildt@higginbotham.net>
2023-01-02 19:36:52 -0500

    added infra tests

--- tests/unit/test_cdk_workshop_stack.py
+++ tests/unit/test_cdk_workshop_stack.py
@@ -3,19 +3,47 @@ import aws_cdk.assertions as assertions
 from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
 
 
-def test_sqs_queue_created():
+# test to confirm that the vpc is created
+def test_vpc_created():
     app = core.App()
     stack = CdkWorkshopStack(app, "cdk-workshop")
     template = assertions.Template.from_stack(stack)
 
-    template.has_resource_properties("AWS::SQS::Queue", {
-        "VisibilityTimeout": 300
-    })
+    template.resource_count_is("AWS::EC2::VPC", 1)
 
-
-def test_sns_topic_created():
+# test to confirm that the security group is created
+def test_security_group_created():
     app = core.App()
     stack = CdkWorkshopStack(app, "cdk-workshop")
     template = assertions.Template.from_stack(stack)
 
-    template.resource_count_is("AWS::SNS::Topic", 1)
+    template.resource_count_is("AWS::EC2::SecurityGroup", 1)
+
+
+# test to confirm that the lambda function is created
+def test_lambda_function_created():
+    app = core.App()
+    stack = CdkWorkshopStack(app, "cdk-workshop")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::Lambda::Function", 2)
+
+# test to confirm that step functions are created
+def test_step_functions_created():
+    app = core.App()
+    stack = CdkWorkshopStack(app, "cdk-workshop")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::StepFunctions::StateMachine", 2)
+
+# test to confirm that the ec2 instances are created
+def test_ec2_instances_created():
+    app = core.App()
+    stack = CdkWorkshopStack(app, "cdk-workshop")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::EC2::Instance", 4)
+
+
+
+
